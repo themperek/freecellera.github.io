73cdf3c26eafb87c42ecabe485e850b5520a0306
Chris Higgs <chris.h@nanospeed.co.uk>
2015-03-02 11:23:14 +0000

    Change hosted site to freecellera.github.io

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://freecellera.org'
+SITEURL = 'http://freecellera.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'

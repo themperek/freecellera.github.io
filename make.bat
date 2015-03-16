a760349df9c6a351e6837673886f355fbcffddd1
Tomasz Hemperek <themperek@gmail.com>
2015-03-16 11:35:45 +0100

    add make.bat for windows

---
+++ make.bat
@@ -0,0 +1,34 @@
+@ECHO OFF
+
+if "%PELICAN%" == "" (
+	set PELICAN=pelican
+)
+
+set INPUTDIR=content
+set OUTPUTDIR=output
+set PUBLISHCONF=publishconf.py
+set PELICANOPTS=
+
+
+if "%1" == "publish" (
+	%PELICAN% %INPUTDIR% -o %OUTPUTDIR% -s %PUBLISHCONF% -t theme %PELICANOPTS% 
+	if errorlevel 1 exit /b 1
+	echo.
+	echo.Build finished. The HTML pages are in %OUTPUTDIR%.
+	goto end
+)
+
+if "%1" == "clean" (
+	rmdir /q /s %OUTPUTDIR%
+	rmdir /q /s cache
+	goto end
+)
+
+if "%1" == "server" (
+	cd %OUTPUTDIR%
+	python -m SimpleHTTPServer
+)
+
+
+
+:end
